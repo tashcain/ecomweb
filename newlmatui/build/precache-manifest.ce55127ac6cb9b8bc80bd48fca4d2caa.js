108102c66cffa95df5b76d564d3ee9ac6b1a42b8
tashcain <shubhambaswal10@gmail.com>
2019-11-15 17:48:34 +0530

    second commit

---
+++ newlmatui/build/precache-manifest.ce55127ac6cb9b8bc80bd48fca4d2caa.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89cb539e1edc22de9d7cdeaf6e8d6c03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd03d56886179bdff317",
+    "url": "/static/css/2.764c1c17.chunk.css"
+  },
+  {
+    "revision": "4deba918c731c491e061",
+    "url": "/static/css/main.bc3cd4e0.chunk.css"
+  },
+  {
+    "revision": "fd03d56886179bdff317",
+    "url": "/static/js/2.677422aa.chunk.js"
+  },
+  {
+    "revision": "4deba918c731c491e061",
+    "url": "/static/js/main.04f6cd9c.chunk.js"
+  },
+  {
+    "revision": "4cf512219cf323a8a5ad",
+    "url": "/static/js/runtime-main.cc22ba9c.js"
+  },
+  {
+    "revision": "b165489f022db2b99161721eff7a9929",
+    "url": "/static/media/BlueTea.b165489f.jpg"
+  },
+  {
+    "revision": "b165489f022db2b99161721eff7a9929",
+    "url": "/static/media/BlueTea1.b165489f.jpg"
+  },
+  {
+    "revision": "2f0ab3a8895ab3b8c8a1fb4d53b74452",
+    "url": "/static/media/PremiumTea.2f0ab3a8.jpg"
+  },
+  {
+    "revision": "1fc2bee082254ae6d4d187a549e4c50f",
+    "url": "/static/media/TulsiGreenTea1.1fc2bee0.jpg"
+  },
+  {
+    "revision": "65102ef59dfd0959f13cbbab1af26aca",
+    "url": "/static/media/assamtea.65102ef5.JPG"
+  },
+  {
+    "revision": "b9e6f77bd8320ad0945f4ddd1662bb7f",
+    "url": "/static/media/carimg1.b9e6f77b.JPG"
+  },
+  {
+    "revision": "939d522735ea0cdb44bfdf4d226e75c1",
+    "url": "/static/media/carimg2.939d5227.JPG"
+  },
+  {
+    "revision": "fe9cc0b0ca0db612768ae45f116d68e2",
+    "url": "/static/media/carimg3.fe9cc0b0.JPG"
+  },
+  {
+    "revision": "c4c0d5676132c31646137e1774d85541",
+    "url": "/static/media/footer.c4c0d567.JPG"
+  },
+  {
+    "revision": "8faa0c9d72fefad51882bb1f98b84890",
+    "url": "/static/media/girl.8faa0c9d.jpg"
+  },
+  {
+    "revision": "4b6d32ffdcf9d7283fd2f5be1318df34",
+    "url": "/static/media/leavesbg.4b6d32ff.JPG"
+  },
+  {
+    "revision": "b2b75581b8220a7d338e37d73e8645b3",
+    "url": "/static/media/leavesbg02.b2b75581.JPG"
+  },
+  {
+    "revision": "7aeea290257eb76e4710fbd1ef513bc1",
+    "url": "/static/media/ptinfo.7aeea290.jpg"
+  },
+  {
+    "revision": "ce1d835e9582df0aa1e90b9a3ee072c9",
+    "url": "/static/media/teabg4.ce1d835e.jpg"
+  },
+  {
+    "revision": "945ff7480c6b13a5d56681c6dde5fd27",
+    "url": "/static/media/tgtinfo.945ff748.jpg"
+  },
+  {
+    "revision": "b3c7e708cac49a34a175bd4d9462bc8d",
+    "url": "/static/media/wholeleafGreentea.b3c7e708.jpg"
+  }
+]);
